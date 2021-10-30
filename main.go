efe14b0c48680ddc9d874eb9d49c4e4847cbd3f4
lei <nusmql@gmail.com>
2021-10-30 23:58:42 +0300

    Add config and log module

--- main.go
+++ main.go
@@ -1,30 +1,68 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"ttstore/router"
-
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/lexkong/log"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"net/http"
+	"time"
+	"ttstore/config"
+	"ttstore/router"
+)
+
+var (
+	cfg = pflag.StringP("config", "c", "", "ttstore config file path.")
 )
 
 func main() {
+	pflag.Parse()
+
+	// init config
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+
+	// set gin mode
+	gin.SetMode(viper.GetString("runmode"))
+
 	// Create the Gin engine.
 	g := gin.New()
 
-	//gin middlewares
 	middlewares := []gin.HandlerFunc{}
 
-	// Routes.
+	// Routes
 	router.Load(
-		// Cores.
+		// Cores
 		g,
-
-		// Middlewares.
+		// Middlewares
 		middlewares...,
 	)
 
-	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
-	log.Printf(http.ListenAndServe(":8088", g).Error())
+	// ping the server to make sure the router is working
+	go func() {
+		if err := pingServer(); err != nil {
+			log.Fatal("The router has no response, or it might took too long to start up.", err)
+		}
+		log.Info("The router has been deployed successfully.")
+	}()
+
+	log.Infof("Start to listening the incoming requests on http address: %s", ":8080")
+	log.Infof(http.ListenAndServe(viper.GetString("addr"), g).Error())
+
+}
+
+func pingServer() error {
+	for i := 0; i < viper.GetInt("max_ping_count"); i++ {
+		res, err := http.Get(viper.GetString("url") + "/sd/health")
+
+		if err == nil && res.StatusCode == 200 {
+			return nil
+		}
+		log.Info("Waiting for the router, retry in 1 second.")
+		time.Sleep(time.Second * 1)
+	}
+	return errors.New("Cannot connect to the router.")
 }
